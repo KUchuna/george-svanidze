987ec1a2f1ce34a54c1656ef9712d2a5abfa669d
Ucha <uchakobakhidze@gmail.com>
2025-01-16 23:01:38 +0400

    fixed eslint error

--- components/ThemeChanger.tsx
+++ components/ThemeChanger.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
 import Image from 'next/image'
 
-const ThemeSwitch = () => {
+export default function ThemeSwitch() {
   const [mounted, setMounted] = useState(false)
   const { setTheme, resolvedTheme } = useTheme()
 
@@ -13,22 +13,21 @@ const ThemeSwitch = () => {
     setMounted(true)
   }, [])
 
-  if (!mounted) {
-    return null
-  }
+  if (!mounted) return null
 
   function handleThemeChange() {
 
-    resolvedTheme === "light" ? setTheme("dark") : setTheme("light")
-
-    console.log(resolvedTheme)
+    if(resolvedTheme === "light") {
+      setTheme("dark")
+    } else {
+      setTheme("light")
+    }
+    
   }
 
   return (
-    <div className="bg-[#232323] w-16 rounded-[32px] h-8 flex items-center px-1 cursor-pointer select-none" onClick={handleThemeChange}>
+    <div className="bg-[#232323] w-16 rounded-[32px] h-8 flex items-center px-1 cursor-pointer select-none" onClick={() => handleThemeChange()}>
       <Image src="/SVGs/whiteball.svg" alt='switch' width={24} height={24} className={`transition-transform duration-150 ${resolvedTheme === "dark" ? "translate-x-full" : ""}`}/>
     </div>
   )
 }
-
-export default ThemeSwitch
