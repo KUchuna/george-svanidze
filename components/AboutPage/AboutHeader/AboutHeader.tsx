f9b2b8e1a2a399e307910d3a36cee127b16c7450
Ucha <uchakobakhidze@gmail.com>
2025-02-23 01:55:19 +0400

    fix eslint warnings

--- components/AboutPage/AboutHeader/AboutHeader.tsx
+++ components/AboutPage/AboutHeader/AboutHeader.tsx
@@ -1,9 +1,8 @@
-/* eslint-disable @next/next/no-img-element */
 
 export default function AboutHeader() {
     return (
         <>
-            <h1 className="font-grotesk text-[4.7vw] leading-none font-medium select-none text-gray-3 dark:text-gray-4">I'm a Digital Product Designer, Enthusiastic,</h1>
+            <h1 className="font-grotesk text-[4.7vw] leading-none font-medium select-none text-gray-3 dark:text-gray-4">I&apos;m a Digital Product Designer, Enthusiastic,</h1>
             <br />
             <div className="px-2 w-full"><div className="w-full h-[1px] bg-[#EBEBEB] dark:bg-[#353535]"></div></div>
             <h1 className="font-grotesk text-[4.7vw] leading-none font-medium select-none text-gray-3 dark:text-gray-4">and <span className="text-black dark:text-white">Father Of Two,</span> living in <span className="text-black dark:text-white">
