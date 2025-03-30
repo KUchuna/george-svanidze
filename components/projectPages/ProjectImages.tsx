38047e23d035dc19a91e8e4bdd6d1bc0699d5846
Ucha Kobakhidze <ukobakhidze@geword.local>
2025-03-30 23:24:32 +0400

    testing placeholders for images

--- components/projectPages/ProjectImages.tsx
+++ components/projectPages/ProjectImages.tsx
@@ -1,8 +1,8 @@
 import NotFound from "@/app/not-found";
 import data from "@/public/lib/projectData"
 import Image from "next/image";
+import fs from "node:fs/promises";
 import { getPlaiceholder } from "plaiceholder";
-import fs from 'node:fs/promises';
 
 export default async function ProjectImages({projectId}: {projectId: number}) {
 
@@ -12,24 +12,27 @@ export default async function ProjectImages({projectId}: {projectId: number}) {
         return <NotFound innerpage/>
     }
 
+
+
     return (
         <main className="flex flex-col p-2 gap-3">
             {project?.images?.map((row, rowIndex) => (
               <div key={rowIndex} className={`flex gap-3 w-full ${row.length > 1 ? "flex-row" : "flex-col"}`}>
               {row.map(async (src, index) => {
-
-                const buffer = await fs.readFile(`./public/${src}`);
-                //const { base64 } = await getPlaiceholder(buffer);
-
+                
+                const buffer = await fs.readFile(`./public/pictures/projects/cardinal/4.png`);
+                const { base64 } = await getPlaiceholder(buffer);
+                
                 return (
                     <Image 
                     key={index}
-                    src={src}
+                    src={src.replace('./public', '')}
                     alt="" 
                     width={100}
                     height={100}
                     className={`h-full ${row.length > 1 ? "flex-1 basis-0" : "w-full"}`}
-                    placeholder="blur"
+                    placeholder='blur'
+                    blurDataURL={base64}
                     unoptimized
                     />
                 )
