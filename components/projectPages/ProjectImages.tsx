1c80dfac0a6e3b27e504dad8696cde921fc41e7c
Ucha <uchakobakhidze@gmail.com>
2025-04-01 22:59:22 +0400

    fixed bugs from previous additions

--- components/projectPages/ProjectImages.tsx
+++ components/projectPages/ProjectImages.tsx
@@ -1,8 +1,6 @@
 import NotFound from "@/app/not-found";
 import data from "@/public/lib/projectData"
 import Image from "next/image";
-import fs from "node:fs/promises";
-import { getPlaiceholder } from "plaiceholder";
 
 export default async function ProjectImages({projectId}: {projectId: number}) {
 
@@ -13,16 +11,11 @@ export default async function ProjectImages({projectId}: {projectId: number}) {
     }
 
 
-
     return (
         <main className="flex flex-col p-2 gap-3">
             {project?.images?.map((row, rowIndex) => (
               <div key={rowIndex} className={`flex gap-3 w-full ${row.length > 1 ? "flex-row" : "flex-col"}`}>
               {row.map(async (src, index) => {
-                
-                const buffer = await fs.readFile(`./public/pictures/projects/cardinal/4.png`);
-                const { base64 } = await getPlaiceholder(buffer);
-                
                 return (
                     <Image 
                     key={index}
@@ -31,8 +24,6 @@ export default async function ProjectImages({projectId}: {projectId: number}) {
                     width={100}
                     height={100}
                     className={`h-full ${row.length > 1 ? "flex-1 basis-0" : "w-full"}`}
-                    placeholder='blur'
-                    blurDataURL={base64}
                     unoptimized
                     />
                 )
