410294f9ab4ecdd9cae8144da6de3bd981d3dbd4
Ucha <uchakobakhidze@gmail.com>
2025-04-05 18:49:39 +0400

    fixed loader colors on dark/light themes

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -1,7 +1,18 @@
+"use client"
 import "./loader.css"
+import { useTheme } from "next-themes";
 
 export default function Loader() {
-    return (
-        <div className="container"></div>
-    )
+  const { theme } = useTheme();
+
+  const color = theme === "dark" ? "white" : "black";
+
+  return (
+    <div
+      className="container"
+      style={{
+        "--uib-color": color,
+      } as React.CSSProperties}
+    />
+  );
 }
