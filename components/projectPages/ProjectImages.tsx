1a0c82e153dc92cc7a28619b02ab7a944a337b6e
Ucha <uchakobakhidze@gmail.com>
2025-04-05 18:31:57 +0400

    testing loader on production

--- components/projectPages/ProjectImages.tsx
+++ components/projectPages/ProjectImages.tsx
@@ -1,37 +1,59 @@
+"use client"
+
 import NotFound from "@/app/not-found";
-import data from "@/public/lib/projectData"
+import data from "@/public/lib/projectData";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import Loader from "../Loader/Loader";
 
-export default async function ProjectImages({projectId}: {projectId: number}) {
+export default function ProjectImages({ projectId }: { projectId: number }) {
+  const project = data.find(p => p.id === projectId);
 
-    const project = data.find(p => p.id == projectId)
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+  const [allImagesLoaded, setAllImagesLoaded] = useState(false);
 
-    if(project == undefined) {
-        return <NotFound innerpage/>
+  const totalImages = project?.images?.flat().length || 0;
+
+  useEffect(() => {
+    if (imagesLoaded === totalImages && totalImages > 0) {
+      setAllImagesLoaded(true);
     }
+  }, [imagesLoaded, totalImages]);
 
+  if (project === undefined) {
+    return <NotFound innerpage />;
+  }
 
-    return (
+  return (
         <main className="flex flex-col p-2 gap-3">
-            {project?.images?.map((row, rowIndex) => (
-              <div key={rowIndex} className={`flex gap-3 w-full ${row.length > 1 ? "flex-row" : "flex-col"}`}>
-              {row.map(async (src, index) => {
-                return (
-                    <Image 
-                    key={index}
-                    src={src.replace('./public', '')}
-                    alt="" 
-                    width={100}
-                    height={100}
-                    className={`h-full ${row.length > 1 ? "flex-1 basis-0" : "w-full"}`}
-                    priority
-                    loading="eager"
-                    unoptimized
-                    />
-                )
-                })}
-              </div>
-          ))}
+            {!allImagesLoaded && (
+                <div className="w-full h-[100vh] flex flex-col justify-center items-center gap-4">
+                    <p className="font-grotesk uppercase text-4xl">Images are loading...</p>
+                    <Loader />
+                </div>
+            )}
+        {project?.images?.map((row, rowIndex) => (
+            <div
+            key={rowIndex}
+            className={`flex gap-3 w-full ${row.length > 1 ? "flex-row" : "flex-col"} ${
+                allImagesLoaded ? "" : "hidden"}`}
+            >
+            {row.map((src, index) => (
+                <Image
+                key={index}
+                src={src.replace('./public', '')}
+                alt=""
+                width={100}
+                height={100}
+                className={`h-full ${row.length > 1 ? "flex-1 basis-0" : "w-full"}`}
+                priority
+                loading="eager"
+                unoptimized
+                onLoad={() => setImagesLoaded((prev) => prev + 1)}
+                />
+            ))}
+            </div>
+        ))}
         </main>
-    )
+  );
 }
