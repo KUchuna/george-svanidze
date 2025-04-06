af0b961607027cb91bb8c94c2f8c708e30e97d49
Ucha <uchakobakhidze@gmail.com>
2025-04-06 22:37:21 +0400

    styling improvements on mobile resolution

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         mainPage
       />
       <main className="text-5xl font-grotesk flex justify-center items-center lg:mt-20 pt-4 md:pt-0 text-center bg-white dark:bg-[#141414] rounded-b-2xl lg:rounded-b-none">
-        <div className="lg:columns-4 lg:block flex flex-col lg:gap-3 gap-5 px-[0.625rem] pb-16 w-full ">
+        <div className="lg:columns-4 lg:block flex flex-col lg:gap-3 gap-5 lg:px-[0.625rem] px-1 lg:pb-16 pb-8 w-full ">
           {data.map((card, index) => {
             return (
               <Card 
